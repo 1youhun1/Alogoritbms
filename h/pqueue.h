269533621ba6b692cf49b1d5fb652c3e279c61b9
shibin wang <1youhun1@gmail.com>
2015-06-01 22:38:58 +0800

    优先级队列

---
+++ h/pqueue.h
@@ -0,0 +1,16 @@
+#ifndef PQUEUE_H
+#define PQUEUE_H
+
+#include "heap.h"
+
+typedef Heap PQueue;
+
+#define pqueue_init heap_init
+#define pqueue_destory heap_destroy
+#define pqueue_insert heap_insert
+#define pqueue_extract heap_extract
+//获取优先级队列中优先级最高的元素
+#define pqueue_peek(pqueue) ((pqueue)->tree == NULL ? NULL : (pqueue)->tree[0])
+#define pqueue_size heap_size
+
+#endif
